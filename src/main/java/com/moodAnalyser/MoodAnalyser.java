ef16b306a701a6a4c8c5d499d4f36fcc3d3131d1
nayab jahan <nayabzafir@gmail.com>
2022-09-07 03:27:48 +0530

    Added:to analyse and Respond happy or sad mood

--- src/main/java/com/moodAnalyser/MoodAnalyser.java
+++ src/main/java/com/moodAnalyser/MoodAnalyser.java
@@ -3,5 +3,11 @@ package com.moodAnalyser;
 public class MoodAnalyser {
     public static void main(String[] args) {
         System.out.println("welcome to Mood Analyser Programme");
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        System.out.println(moodAnalyser.analyseMood("Hi, I Am Nayab , I am sad"));
+    }
+
+    public String analyseMood(String message) {
+        return "sad";
     }
 }
