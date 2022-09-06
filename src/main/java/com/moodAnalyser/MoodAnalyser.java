a72c60c3cffe508e64b0195e74dea3046ea9697f
nayab jahan <nayabzafir@gmail.com>
2022-09-07 03:09:42 +0530

    Added:Mood Analyser programme

---
+++ src/main/java/com/moodAnalyser/MoodAnalyser.java
@@ -0,0 +1,7 @@
+package com.moodAnalyser;
+
+public class MoodAnalyser {
+    public static void main(String[] args) {
+        System.out.println("welcome to Mood Analyser Programme");
+    }
+}
---
+++ src/test/java/com/moodAnalyser/MoodAnalyserTest.java
@@ -0,0 +1,4 @@
+package com.moodAnalyser;
+
+public class MoodAnalyserTest {
+}
